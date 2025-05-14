a0c3e66f7eeb50eb4a5dbcd229e743050cce8d6d
Miguel-Huaranca-C <miktejere@gmail.com>
2025-05-13 22:59:10 -0400

    Fotos de cada eropuerto en la Busqueda de aeropuertos

--- src/app/components/filtroBusqueda/modalFiltroAeropuerto.tsx
+++ src/app/components/filtroBusqueda/modalFiltroAeropuerto.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 interface Aeropuerto {
   idaeropuerto: number;
   nombre: string;
+ // imagen: string; // Nueva propiedad //** */
+ imagen: string; // nueva propiedad
 }
 
 interface ModalFiltroAeropuertoProps {
@@ -84,7 +86,9 @@ const ModalFiltroAeropuerto: React.FC<ModalFiltroAeropuertoProps> = ({
         />
 
         {sugerencias.length > 0 && (
-          <ul className="max-h-40 overflow-y-auto border border-gray-200 rounded bg-white shadow-sm">
+
+          <ul className="max-h-40 overflow-y-auto border border-gray-200 rounded bg-white shadow-sm"> {sugerencias.map((aeropuerto) => ( <li key={aeropuerto.idaeropuerto} className="flex items-center space-x-3 px-4 py-2 cursor-pointer hover:bg-orange-500 hover:text-white" onClick={() => { setAeropuertoTemporal(aeropuerto); setInputTexto(aeropuerto.nombre); setSugerencias([]); }} > <img src={aeropuerto.imagen} alt={aeropuerto.nombre} className="w-10 h-10 object-cover rounded" /> <span>{aeropuerto.nombre}</span> </li> ))} </ul>
+          /*<ul className="max-h-40 overflow-y-auto border border-gray-200 rounded bg-white shadow-sm">
             {sugerencias.map((aeropuerto) => (
               <li
                 key={aeropuerto.idaeropuerto}
@@ -98,7 +102,7 @@ const ModalFiltroAeropuerto: React.FC<ModalFiltroAeropuertoProps> = ({
                 {aeropuerto.nombre}
               </li>
             ))}
-          </ul>
+          </ul>*/
         )}
 
         {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
